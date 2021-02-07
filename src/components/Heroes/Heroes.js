9d43748e0592a276f21b58a974e84d5b16811ba0
Bryan <bryan.cedeo@gmail.com>
2021-02-06 21:10:43 -0500

    resuelto bug con useRef

--- src/components/Heroes/Heroes.js
+++ src/components/Heroes/Heroes.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useRef} from 'react';
+import React from 'react';
 //import styles
 import './Heroes.css';
 //import Custom Hooks
@@ -8,28 +8,20 @@ import { Loading } from './layouts/Loading/Loading';
 import { ListHeroes } from './layouts/heroes/ListHeroes';
 
 export const Heroes = ({
-    url='https://www.superheroapi.com/api.php/1860537170767427/search/1',
+    url = 'https://www.superheroapi.com/api.php/1860537170767427/search/1',
     publisher
 }) => {
-    const isMounted = useRef(true);
     const [loading, data] = useFetch(url);
-    useEffect(()=>{
-        return ()=>{
-            isMounted.current = false
-        }
-    },[])
     return (
         <div className="heroes-container">
-            {   isMounted?
-                    loading ?
-                        <Loading />
-                        :
-                        publisher?
-                            <ListHeroes heroes={data.filter(e=> e.biography.publisher === publisher)} />
-                        :
-                            <ListHeroes heroes={data? data: []} />
+            {
+                loading ?
+                    <Loading />
                 :
-                null
+                    publisher ?
+                        <ListHeroes heroes={data.filter(e => e.biography.publisher === publisher)} />
+                    :
+                        <ListHeroes heroes={data ? data : []} />
             }
         </div>
     );
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,21 +1,29 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 
-export const useFetch = (initial_url)=>{
+export const useFetch = (initial_url) => {
+    const isMounted = useRef(true);
     const [url, setUrl] = useState(initial_url);
     const [loading, setLoading] = useState(true);
-    const [data,setData] = useState([]);
-    useEffect(()=>{
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        return () => {
+            isMounted.current = false
+        }
+    }, [])
+    useEffect(() => {
         fetch(url)
-        .then((res)=>{
-            return res.json();
-        })
-        .then((res)=>{
-            setData(res.results);
-            setLoading(false)
-        })
-    },[url]);
-    return [loading,data,setUrl];
+            .then((res) => {
+                return res.json();
+            })
+            .then((res) => {
+                if (isMounted.current) {
+                    setData(res.results);
+                    setLoading(false)
+                }
+            })
+    }, [url]);
+    return [loading, data, setUrl];
 }
 
 useFetch.propTypes = {
